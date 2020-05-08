e6b212f8ccc01264cca88597150fe570ddd5b98b
FurtiveShadows <61507092+FurtiveShadows@users.noreply.github.com>
2020-05-08 19:34:24 -0400

    Settings close

--- Assets/RocketMouse/Scripts/UIManager.cs
+++ Assets/RocketMouse/Scripts/UIManager.cs
@@ -8,6 +8,8 @@ public class UIManager : MonoBehaviour
 	
 	public Animator startButton;
 	public Animator settingsButton;
+	public Animator dialog;
+
 
 	
 	public void StartGame() 
@@ -19,7 +21,17 @@ public class UIManager : MonoBehaviour
 	{
 		startButton.SetBool("isHidden", true);
 		settingsButton.SetBool("isHidden", true);
+		
+		dialog.SetBool("isHidden", false);
 	}
+	
+	public void CloseSettings() 
+	{
+		startButton.SetBool("isHidden", false);
+		settingsButton.SetBool("isHidden", false);
+		dialog.SetBool("isHidden", true);
+	}
+
 
 	
 }
